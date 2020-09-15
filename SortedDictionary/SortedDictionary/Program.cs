36d31dca8722748f690c8ee746fdec4cd093b82c
gilvan <g.r.almeida@live.com>
2020-09-15 12:02:53 -0300

    SortedDictionary

---
+++ SortedDictionary/SortedDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, Aluno> alunos =
+                new SortedDictionary<string, Aluno>();
+
+            alunos.Add("VT", new Aluno("Vanessa", 34672));
+            alunos.Add("AL", new Aluno("Ana", 5617));
+            alunos.Add("RN", new Aluno("Rafael", 17645));
+            alunos.Add("WM", new Aluno("Wanderson", 11287));
+
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
